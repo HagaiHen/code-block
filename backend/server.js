82797fa48c3518b1a6d4bd9023e1b6dff620803b
hagai_hen <hagai349@gmail.com>
2024-07-22 12:38:32 +0300

    deploy changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 import authRoutes from "./routes/auth.js";
@@ -7,13 +8,21 @@ import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config(); // Loading environment variables from a .env file
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 app.use(express.json()); // Middleware to parse incoming requests with JSON payloads (req.body)
 app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
 app.use("/api/codeblocks", codeBlocksRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+})
 
 // Starting the server and connecting to MongoDB
 server.listen(PORT, () => {
